2c47184884f4a684ba63a5c37d37aeea6e6872ad
LloydLion <55992792+LloydLion@users.noreply.github.com>
2021-06-30 18:51:12 +0300

    Добавьте файлы проекта.

---
+++ MathCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MathCore
+{
+	public class Class1
+	{
+	}
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TestProject
+{
+	public class Tests
+	{
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
